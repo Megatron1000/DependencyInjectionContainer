db3869e5119b515abe0125822dee443b630db327
Mark Bridges <mark@bridgetech.io>
2020-04-02 11:36:08 +0100

    Initial Commit

---
+++ DependencyInjectionContainer/DependencyInjectionContainer.h
@@ -0,0 +1,19 @@
+//
+//  DependencyInjectionContainer.h
+//  DependencyInjectionContainer
+//
+//  Created by Mark Bridges on 02/04/2020.
+//  Copyright © 2020 BridgeTech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DependencyInjectionContainer.
+FOUNDATION_EXPORT double DependencyInjectionContainerVersionNumber;
+
+//! Project version string for DependencyInjectionContainer.
+FOUNDATION_EXPORT const unsigned char DependencyInjectionContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DependencyInjectionContainer/PublicHeader.h>
+
+
